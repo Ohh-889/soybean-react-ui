bee90f766679e1894e420753adbad8396b46ecfb
Ohh <1509326266@qq.com>
2025-07-03 23:59:23 +0800

    feat: add  other type

---
+++ packages/ui-variants/src/index.ts
@@ -0,0 +1,7 @@
+export * from './shared';
+
+export * from './types';
+
+export * from './variants/button';
+
+export * from './variants/card';
---
+++ packages/ui-variants/src/types/other.ts
@@ -0,0 +1,8 @@
+import { type ClassValue } from 'clsx';
+
+export type ClassValueProp = {
+  /** class name */
+  className?: ClassValue;
+};
+
+export { ClassValue };
