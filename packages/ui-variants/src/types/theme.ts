9326529be7233fd7a97b0b8a53d6b4079f1254e6
Ohh <1509326266@qq.com>
2025-07-03 23:58:39 +0800

    feat: add theme type

---
+++ packages/ui-variants/src/types/theme.ts
@@ -0,0 +1,9 @@
+export type ThemeColor = 'accent' | 'carbon' | 'destructive' | 'info' | 'primary' | 'secondary' | 'success' | 'warning';
+
+export type ThemeSize = '2xl' | 'lg' | 'md' | 'sm' | 'xl' | 'xs';
+
+export type ThemeOrientation = 'horizontal' | 'vertical';
+
+export type ThemeAlign = 'center' | 'end' | 'start';
+
+export type ThemeSide = 'bottom' | 'left' | 'right' | 'top';
