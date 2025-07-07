9d1114b94d2c396a0430a7400e867a2aa1e6b6bd
Ohh <1509326266@qq.com>
2025-07-07 11:35:04 +0800

    feat: modify the Tailwind configuration of the demo

--- playground/tailwind.config.ts
+++ playground/tailwind.config.ts
@@ -1,35 +1,9 @@
-import path from 'node:path';
-
+import { soybeanUIPlugin } from 'skyroc-tailwind-plugin';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', '../../packages/ui/src/**/*.{ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        accent: {
-          DEFAULT: 'var(--accent)',
-          foreground: 'var(--accent-foreground)'
-        },
-        background: 'var(--background)',
-        destructive: {
-          DEFAULT: 'var(--destructive)',
-          foreground: 'var(--destructive-foreground)'
-        },
-        foreground: 'var(--foreground)',
-        input: 'var(--input)',
-        primary: {
-          DEFAULT: 'var(--primary)',
-          foreground: 'var(--primary-foreground)'
-        },
-        ring: 'var(--ring)',
-        secondary: {
-          DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)'
-        }
-      }
-    }
-  }
+  darkMode: 'class',
+  plugins: [soybeanUIPlugin()]
 };
 
 export default config;
