273032c764bc8218b888a68f93a636d7a10109e2
Ohh <1509326266@qq.com>
2025-07-07 11:36:46 +0800

    feat: add base componet type

---
+++ packages/ui/src/types/other.ts
@@ -0,0 +1,41 @@
+import type { ClassValue } from 'clsx';
+import type { ReactNode } from 'react';
+
+export type AsTag =
+  | 'a'
+  | 'button'
+  | 'div'
+  | 'form'
+  | 'h2'
+  | 'h3'
+  | 'img'
+  | 'input'
+  | 'label'
+  | 'li'
+  | 'nav'
+  | 'ol'
+  | 'p'
+  | 'span'
+  | 'svg'
+  | 'ul'
+  | 'template'
+  | ({} & string);
+
+export interface PrimitiveProps {
+  /**
+   * The element or component this component should render as. Can be overwrite by `asChild`
+   *
+   * @defaultValue 'div'
+   */
+  as?: AsTag | ReactNode;
+  /**
+   * Change the default rendered element for the one passed as a child, merging their props and behavior.
+   *
+   * Read our [Composition](https://www.soybean-ui.com/docs/guides/composition) guide for more details.
+   */
+  asChild?: boolean;
+}
+
+export type BaseProps<T> = Omit<T, 'className'> & {
+  className?: ClassValue;
+};
