09abccd49e70020f7ca21f433983c8e025841a8c
Ohh <1509326266@qq.com>
2025-07-07 11:36:26 +0800

    feat: add button-group component

--- packages/ui-variants/src/index.ts
+++ packages/ui-variants/src/index.ts
@@ -3,5 +3,6 @@ export * from './shared';
 export * from './types';
 
 export * from './variants/button';
-
+export * from './variants/button-group';
 export * from './variants/card';
+export * from './variants/tabs';
---
+++ packages/ui/src/components/button/index.ts
@@ -0,0 +1,4 @@
+export * from './source/Button';
+export * from './source/ButtonGroup';
+
+export * from './type';
