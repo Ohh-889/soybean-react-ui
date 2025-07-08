d8be3bd95e576c3f005e3224adeea86d55a31790
Ohh <1509326266@qq.com>
2025-07-08 18:39:39 +0800

    feat: add primitives module id subpkg

---
+++ primitives/id/src/id.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { useLayoutEffect } from 'soybean-react-ui/use-layout-effect';
+
+// We spaces with `.trim().toString()` to prevent bundlers from trying to `import { useId } from 'react';`
+const useReactId = (React as any)[' useId '.trim().toString()] || (() => undefined);
+let count = 0;
+
+function useId(deterministicId?: string): string {
+  const [id, setId] = React.useState<string | undefined>(useReactId());
+  // React versions older than 18 will have client-side ids only.
+  useLayoutEffect(() => {
+    if (!deterministicId) setId(reactId => reactId ?? String((count += 1)));
+  }, [deterministicId]);
+  return deterministicId || (id ? `soybean-${id}` : '');
+}
+
+export { useId };
