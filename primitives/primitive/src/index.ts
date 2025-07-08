5eba970c9e1d2265f92b712222ec1514bf027f10
Ohh <1509326266@qq.com>
2025-07-08 18:36:23 +0800

    feat: add primitives module primitive subpkg

---
+++ primitives/primitive/src/index.ts
@@ -0,0 +1,8 @@
+export {
+  Primitive,
+  //
+  Root
+} from './primitive';
+export type { NODES, PrimitivePropsWithRef } from './primitive';
+
+export { composeEventHandlers, dispatchDiscreteCustomEvent } from './util';
