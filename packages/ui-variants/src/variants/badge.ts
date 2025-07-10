634c04fe084ba5a8a3d136e9ce16f9e8f2886887
Ohh <1509326266@qq.com>
2025-07-10 17:40:07 +0800

    Revert "style: lint code"
    This reverts commit 2de5b5e8d54a6245c8ad7bd8e6884291215ba8b6.

--- packages/ui-variants/src/variants/badge.ts
+++ packages/ui-variants/src/variants/badge.ts
@@ -3,125 +3,125 @@ import type { VariantProps } from 'tailwind-variants';
 
 export const badgeVariants = tv({
   base: 'inline-flex items-center border font-semibold',
-  compoundVariants: [
-    {
-      class: `bg-primary text-primary-foreground`,
-      color: 'primary',
-      variant: 'solid'
-    },
-    {
-      class: `bg-destructive text-destructive-foreground`,
-      color: 'destructive',
-      variant: 'solid'
-    },
-    {
-      class: `bg-success text-success-foreground`,
-      color: 'success',
-      variant: 'solid'
-    },
-    {
-      class: `bg-warning text-warning-foreground`,
-      color: 'warning',
-      variant: 'solid'
-    },
-    {
-      class: `bg-info text-info-foreground`,
-      color: 'info',
-      variant: 'solid'
-    },
-    {
-      class: `bg-carbon text-carbon-foreground`,
-      color: 'carbon',
-      variant: 'solid'
-    },
-    {
-      class: `bg-secondary text-secondary-foreground`,
-      color: 'secondary',
-      variant: 'solid'
-    },
-    {
-      class: `bg-accent text-accent-foreground`,
-      color: 'accent',
-      variant: 'solid'
-    },
-    {
-      class: 'bg-primary/10',
-      color: 'primary',
-      variant: ['soft', 'ghost']
-    },
-    {
-      class: 'bg-destructive/10',
-      color: 'destructive',
-      variant: ['soft', 'ghost']
-    },
-    {
-      class: 'bg-success/10',
-      color: 'success',
-      variant: ['soft', 'ghost']
-    },
-    {
-      class: 'bg-warning/10',
-      color: 'warning',
-      variant: ['soft', 'ghost']
-    },
-    {
-      class: 'bg-info/10',
-      color: 'info',
-      variant: ['soft', 'ghost']
-    },
-    {
-      class: 'bg-carbon/10',
-      color: 'carbon',
-      variant: ['soft', 'ghost']
-    },
-    {
-      class: 'bg-secondary-foreground/5',
-      color: 'secondary',
-      variant: ['soft', 'ghost']
-    },
-    {
-      class: 'bg-accent-foreground/5',
-      color: 'accent',
-      variant: ['soft', 'ghost']
-    }
-  ],
-  defaultVariants: {
-    color: 'primary',
-    shape: 'auto',
-    size: 'md',
-    variant: 'solid'
-  },
   variants: {
     color: {
-      accent: 'border-accent-foreground/50 text-accent-foreground',
-      carbon: 'border-carbon text-carbon',
-      destructive: 'border-destructive text-destructive',
-      info: 'border-info text-info',
       primary: 'border-primary text-primary',
-      secondary: 'border-secondary-foreground/50 text-secondary-foreground',
+      destructive: 'border-destructive text-destructive',
       success: 'border-success text-success',
-      warning: 'border-warning text-warning'
+      warning: 'border-warning text-warning',
+      info: 'border-info text-info',
+      carbon: 'border-carbon text-carbon',
+      secondary: 'border-secondary-foreground/50 text-secondary-foreground',
+      accent: 'border-accent-foreground/50 text-accent-foreground'
+    },
+    variant: {
+      solid: '',
+      pure: 'bg-background text-foreground border-border',
+      outline: 'bg-background',
+      soft: 'border-0',
+      ghost: '',
+      raw: 'bg-transparent border-0'
+    },
+    size: {
+      xs: 'gap-0.5 h-3.5 px-1 text-4xs',
+      sm: 'gap-0.75 h-4 px-1.5 text-3xs',
+      md: 'gap-1 h-4.5 px-2 text-2xs',
+      lg: 'gap-1.25 h-5 px-2.5 text-xs',
+      xl: 'gap-1.5 h-6 px-3 text-sm',
+      '2xl': 'gap-2 h-7 px-4 text-base'
     },
     shape: {
       auto: 'rounded-md',
       rounded: 'rounded-full'
-    },
-    size: {
-      '2xl': 'gap-2 h-7 px-4 text-base',
-      lg: 'gap-1.25 h-5 px-2.5 text-xs',
-      md: 'gap-1 h-4.5 px-2 text-2xs',
-      sm: 'gap-0.75 h-4 px-1.5 text-3xs',
-      xl: 'gap-1.5 h-6 px-3 text-sm',
-      xs: 'gap-0.5 h-3.5 px-1 text-4xs'
-    },
-    variant: {
-      ghost: '',
-      outline: 'bg-background',
-      pure: 'bg-background text-foreground border-border',
-      raw: 'bg-transparent border-0',
-      soft: 'border-0',
-      solid: ''
     }
+  },
+  compoundVariants: [
+    {
+      color: 'primary',
+      variant: 'solid',
+      class: `bg-primary text-primary-foreground`
+    },
+    {
+      color: 'destructive',
+      variant: 'solid',
+      class: `bg-destructive text-destructive-foreground`
+    },
+    {
+      color: 'success',
+      variant: 'solid',
+      class: `bg-success text-success-foreground`
+    },
+    {
+      color: 'warning',
+      variant: 'solid',
+      class: `bg-warning text-warning-foreground`
+    },
+    {
+      color: 'info',
+      variant: 'solid',
+      class: `bg-info text-info-foreground`
+    },
+    {
+      color: 'carbon',
+      variant: 'solid',
+      class: `bg-carbon text-carbon-foreground`
+    },
+    {
+      color: 'secondary',
+      variant: 'solid',
+      class: `bg-secondary text-secondary-foreground`
+    },
+    {
+      color: 'accent',
+      variant: 'solid',
+      class: `bg-accent text-accent-foreground`
+    },
+    {
+      color: 'primary',
+      variant: ['soft', 'ghost'],
+      class: 'bg-primary/10'
+    },
+    {
+      color: 'destructive',
+      variant: ['soft', 'ghost'],
+      class: 'bg-destructive/10'
+    },
+    {
+      color: 'success',
+      variant: ['soft', 'ghost'],
+      class: 'bg-success/10'
+    },
+    {
+      color: 'warning',
+      variant: ['soft', 'ghost'],
+      class: 'bg-warning/10'
+    },
+    {
+      color: 'info',
+      variant: ['soft', 'ghost'],
+      class: 'bg-info/10'
+    },
+    {
+      color: 'carbon',
+      variant: ['soft', 'ghost'],
+      class: 'bg-carbon/10'
+    },
+    {
+      color: 'secondary',
+      variant: ['soft', 'ghost'],
+      class: 'bg-secondary-foreground/5'
+    },
+    {
+      color: 'accent',
+      variant: ['soft', 'ghost'],
+      class: 'bg-accent-foreground/5'
+    }
+  ],
+  defaultVariants: {
+    color: 'primary',
+    variant: 'solid',
+    size: 'md',
+    shape: 'auto'
   }
 });
 
--- packages/ui-variants/src/variants/divider.ts
+++ packages/ui-variants/src/variants/divider.ts
@@ -2,155 +2,155 @@ import { tv } from 'tailwind-variants';
 import type { VariantProps } from 'tailwind-variants';
 
 export const dividerVariants = tv({
-  compoundVariants: [
-    {
-      class: {
-        label: 'px-0.75 py-1.5'
-      },
-      orientation: 'vertical',
-      size: 'xs'
-    },
-    {
-      class: {
-        label: 'px-1.5 py-0.75'
-      },
-      orientation: 'horizontal',
-      size: 'xs'
-    },
-    {
-      class: {
-        label: 'px-0.875 py-1.75'
-      },
-      orientation: 'vertical',
-      size: 'sm'
-    },
-    {
-      class: {
-        label: 'px-1.75 py-0.875'
-      },
-      orientation: 'horizontal',
-      size: 'sm'
-    },
-    {
-      class: {
-        label: 'px-1 py-2'
-      },
-      orientation: 'vertical',
-      size: 'md'
-    },
-    {
-      class: {
-        label: 'px-2 py-1'
-      },
-      orientation: 'horizontal',
-      size: 'md'
-    },
-    {
-      class: {
-        label: 'px-1.125 py-2.25'
-      },
-      orientation: 'vertical',
-      size: 'lg'
-    },
-    {
-      class: {
-        label: 'px-2.25 py-1.125'
-      },
-      orientation: 'horizontal',
-      size: 'lg'
-    },
-    {
-      class: {
-        label: 'px-1.25 py-2.5'
-      },
-      orientation: 'vertical',
-      size: 'xl'
-    },
-    {
-      class: {
-        label: 'px-2.5 py-1.25'
-      },
-      orientation: 'horizontal',
-      size: 'xl'
-    },
-    {
-      class: {
-        label: 'px-1.5 py-3'
-      },
-      orientation: 'vertical',
-      size: '2xl'
-    },
-    {
-      class: {
-        label: 'px-3 py-1.5'
-      },
-      orientation: 'horizontal',
-      size: '2xl'
-    }
-  ],
-  defaultVariants: {
-    align: 'center',
-    border: 'solid',
-    orientation: 'horizontal',
-    size: 'md'
-  },
   slots: {
-    label: `absolute top-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center text-muted-foreground bg-background`,
-    root: `relative shrink-0 border-border`
+    root: `relative shrink-0 border-border`,
+    label: `absolute top-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center text-muted-foreground bg-background`
   },
   variants: {
+    size: {
+      xs: {
+        label: 'text-2xs'
+      },
+      sm: {
+        label: 'text-xs'
+      },
+      md: {
+        label: 'text-sm'
+      },
+      lg: {
+        label: 'text-base'
+      },
+      xl: {
+        label: 'text-lg'
+      },
+      '2xl': {
+        label: 'text-2xl'
+      }
+    },
+    orientation: {
+      vertical: {
+        root: 'h-full border-l',
+        label: 'w-[1px]'
+      },
+      horizontal: {
+        root: 'w-full border-t',
+        label: 'h-[1px]'
+      }
+    },
     align: {
+      start: {
+        label: 'left-1/16 sm:left-1/8 md:left-1/10 lg:left-1/12'
+      },
       center: {
         label: 'left-1/2'
       },
       end: {
-        label: 'left-15/16 max-sm:left-7/8 max-md:left-9/10 max-lg:left-11/12'
-      },
-      start: {
-        label: 'left-1/16 max-sm:left-1/8 max-md:left-1/10 max-lg:left-1/12'
+        label: 'left-15/16 sm:left-7/8 md:left-9/10 lg:left-11/12'
       }
     },
     border: {
+      solid: {
+        root: 'border-solid'
+      },
       dashed: {
         root: 'border-dashed'
       },
       dotted: {
         root: 'border-dotted'
-      },
-      solid: {
-        root: 'border-solid'
-      }
-    },
-    orientation: {
-      horizontal: {
-        label: 'h-[1px]',
-        root: 'w-full border-t'
-      },
-      vertical: {
-        label: 'w-[1px]',
-        root: 'h-full border-l'
-      }
-    },
-    size: {
-      '2xl': {
-        label: 'text-2xl'
-      },
-      lg: {
-        label: 'text-base'
-      },
-      md: {
-        label: 'text-sm'
-      },
-      sm: {
-        label: 'text-xs'
-      },
-      xl: {
-        label: 'text-lg'
-      },
-      xs: {
-        label: 'text-2xs'
       }
     }
+  },
+  compoundVariants: [
+    {
+      size: 'xs',
+      orientation: 'vertical',
+      class: {
+        label: 'px-0.75 py-1.5'
+      }
+    },
+    {
+      size: 'xs',
+      orientation: 'horizontal',
+      class: {
+        label: 'px-1.5 py-0.75'
+      }
+    },
+    {
+      size: 'sm',
+      orientation: 'vertical',
+      class: {
+        label: 'px-0.875 py-1.75'
+      }
+    },
+    {
+      size: 'sm',
+      orientation: 'horizontal',
+      class: {
+        label: 'px-1.75 py-0.875'
+      }
+    },
+    {
+      size: 'md',
+      orientation: 'vertical',
+      class: {
+        label: 'px-1 py-2'
+      }
+    },
+    {
+      size: 'md',
+      orientation: 'horizontal',
+      class: {
+        label: 'px-2 py-1'
+      }
+    },
+    {
+      size: 'lg',
+      orientation: 'vertical',
+      class: {
+        label: 'px-1.125 py-2.25'
+      }
+    },
+    {
+      size: 'lg',
+      orientation: 'horizontal',
+      class: {
+        label: 'px-2.25 py-1.125'
+      }
+    },
+    {
+      size: 'xl',
+      orientation: 'vertical',
+      class: {
+        label: 'px-1.25 py-2.5'
+      }
+    },
+    {
+      size: 'xl',
+      orientation: 'horizontal',
+      class: {
+        label: 'px-2.5 py-1.25'
+      }
+    },
+    {
+      size: '2xl',
+      orientation: 'vertical',
+      class: {
+        label: 'px-1.5 py-3'
+      }
+    },
+    {
+      size: '2xl',
+      orientation: 'horizontal',
+      class: {
+        label: 'px-3 py-1.5'
+      }
+    }
+  ],
+  defaultVariants: {
+    size: 'md',
+    orientation: 'horizontal',
+    align: 'center',
+    border: 'solid'
   }
 });
 
