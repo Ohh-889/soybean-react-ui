1b010b3197c0699338197fed5aac51367b163631
Ohh-889 <1509326266@qq.com>
2025-07-14 04:06:46 +0800

    feat: optimize sonner demo

--- playground/src/app/(demo)/sonner/page.tsx
+++ playground/src/app/(demo)/sonner/page.tsx
@@ -1,32 +1,9 @@
 'use client';
-import { Button, toast, useSonner } from 'soybean-react-ui';
+import { toast } from 'soybean-react-ui';
 
 const SonnerPage = () => {
-  const a = useSonner();
-
   return (
     <div>
-      <button
-        onClick={() =>
-          toast('Hello', {
-            duration: 100000
-          })
-        }
-      >
-        Click me
-      </button>
-
-      <button
-        onClick={() =>
-          toast.success('Hello', {
-            description: 'This is a description',
-            duration: 100000
-          })
-        }
-      >
-        Click me
-      </button>
-
       <button
         onClick={() =>
           toast.success('Hello', {
