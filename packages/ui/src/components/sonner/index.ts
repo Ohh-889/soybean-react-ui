2984a3597c11bf1268a615b4c6596c0ac5cef870
Ohh-889 <1509326266@qq.com>
2025-07-16 13:27:48 +0800

    fix: fix the error of component import

--- packages/ui/src/components/sonner/index.ts
+++ packages/ui/src/components/sonner/index.ts
@@ -1,5 +1,5 @@
 export { toast, useSonner } from 'sonner';
 
-export { default as Sonner } from './source/sonner';
+export { default as Sonner } from './sonner';
 
 export * from './types';
--- packages/ui/src/components/tabs/TabsList.tsx
+++ packages/ui/src/components/tabs/TabsList.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { useComposedRefs } from '@radix-ui/react-compose-refs';
 import { List } from '@radix-ui/react-tabs';
-import { cn, tabsVariants } from '@soybean-react-ui/variants';
 import type { CSSProperties } from 'react';
 import { forwardRef, useEffect, useRef, useState } from 'react';
 
+import { cn } from '@/lib/utils';
+
 import { If } from '../if';
 
+import { tabsVariants } from './tabs-variants';
 import type { IndicatorStyle, TabsListProps } from './types';
 
 const TabsList = forwardRef<React.ElementRef<typeof List>, TabsListProps>((props, ref) => {
