56b8b070afe6ffae4a346a0cb9c58924fade0e8b
Ohh-889 <1509326266@qq.com>
2025-08-04 02:44:03 +0800

    feat: delete home page demo button

--- playground/src/app/page.tsx
+++ playground/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from 'soybean-react-ui';
 
@@ -16,22 +15,6 @@ export default function Home() {
       <Button asChild>
         <Link href="/tabs">Tabs</Link>
       </Button>
-
-      <a
-        className="flex items-center gap-2 border font-medium text-sm justify-center rounded-full border-solid border-transparent transition-colors bg-foreground text-background hover:bg-[#383838] dark:hover:bg-[#ccc] sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <Image
-          alt="Vercel logomark"
-          className="dark:invert"
-          height={20}
-          src="/vercel.svg"
-          width={20}
-        />
-        Deploy now
-      </a>
     </div>
   );
 }
