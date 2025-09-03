987d715febb89edf58d379d647585119f0dd26e7
Ohh-889 <1509326266@qq.com>
2025-09-03 14:24:55 +0800

    chore: Modify the unified configuration

--- internal/create-skyroc/template-ui-primitives/tsdown.config.ts
+++ internal/create-skyroc/template-ui-primitives/tsdown.config.ts
@@ -4,13 +4,7 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
   minify: false,
-  outExtensions: ctx => {
-    return {
-      js: ctx.format === 'cjs' ? '.cjs' : '.mjs'
-    };
-  },
   platform: 'neutral',
   shims: true,
   sourcemap: false
