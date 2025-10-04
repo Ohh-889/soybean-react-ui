06228588d9f129f8a6a67b5f7e430eed99e1baa5
Ohh-889 <1509326266@qq.com>
2025-10-05 00:35:48 +0800

    chore(tailwind): update theme option types, simplify CSS variable generation logic, and optimize plugin export structure

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import { soybeanUIPlugin } from '@soybean-react-ui/tailwind-plugin';
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  darkMode: 'class',
-  plugins: [soybeanUIPlugin()]
-};
-
-export default config;
