104f6112eaebb4de20360235eb660abc2aec77f4
Ohh-889 <1509326266@qq.com>
2025-10-05 21:05:25 +0800

    chore(ui-kit): remove unused files including CHANGELOG, package.json, README, and config files

--- internal/ui-kit/src/core/logger.ts
+++
@@ -1,8 +0,0 @@
-import pc from 'picocolors';
-
-export const log = {
-  err: (msg: string) => console.error(pc.red('err  ') + msg),
-  info: (msg: string) => console.log(pc.cyan('info ') + msg),
-  ok: (msg: string) => console.log(pc.green('ok   ') + msg),
-  warn: (msg: string) => console.log(pc.yellow('warn ') + msg)
-};
